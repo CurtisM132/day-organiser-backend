5eced11f323f69f2c49faf6271ef400ae108cff3
Curtis Martin <curtisastonmartin@hotmail.co.uk>
2021-08-26 21:49:55 +0100

    Foundational Code

---
+++ Controllers/SpacedRepetitionController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using day_organiser_backend.Entities;
+using day_organiser_backend.Repositories;
+
+namespace day_organiser_backend.Controllers
+{
+    [ApiController]
+    [Route("spaced-repetition")]
+    public class SpacedRepetitionController : ControllerBase
+    {
+        //private readonly ILogger<SpacedRepetitionController> _logger;
+        //public SpacedRepetitionController(ILogger<SpacedRepetitionController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly InMemSpacedRepetitionRepo repo;
+
+        public SpacedRepetitionController()
+        {
+            repo = new InMemSpacedRepetitionRepo();
+        }
+
+        // GET spaced-repetition
+        [HttpGet]
+        public IEnumerable<SpacedRepetitionRecord> Get()
+        {
+            return repo.GetRecords();
+        }
+
+        // GET spaced-repetitio/{id}
+        [HttpGet("{id}")]
+        public SpacedRepetitionRecord Get(Guid id)
+        {
+            return repo.GetRecord(id);
+        }
+    }
+}
---
+++ Repositories/InMemSpacedRepetitionRepo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using day_organiser_backend.Entities;
+
+namespace day_organiser_backend.Repositories
+{
+    public class InMemSpacedRepetitionRepo
+    {
+
+        private readonly List<SpacedRepetitionRecord> records = new()
+        {
+            new SpacedRepetitionRecord
+            {
+                Id = Guid.NewGuid(),
+                Topic = "RESTful APIs",
+                LearningHistory = new SpacedRepetitionLearningRecord[]
+            {
+                new SpacedRepetitionLearningRecord {
+                    Id = Guid.NewGuid(),
+                    Date = DateTimeOffset.UtcNow,
+                    Proficiency = LearningProficiency.Low
+                }
+            },
+            }
+        };
+
+        public IEnumerable<SpacedRepetitionRecord> GetRecords()
+        {
+            return records;
+        }
+
+        public SpacedRepetitionRecord GetRecord(Guid id)
+        {
+            return records.Where(item => item.Id == id).SingleOrDefault();
+        }
+
+    }
+}
